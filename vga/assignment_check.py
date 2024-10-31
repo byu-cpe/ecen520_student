e5285c73275faf900b57ca1214ba9124a5273367
wirthlin <wirthlin@byu.edu>
2024-10-31 16:16:08 -0600

    Fixed vga assignment check

--- vga/assignment_check.py
+++ vga/assignment_check.py
@@ -15,7 +15,7 @@ import repo_test
 def main():
 
     tester = test_suite_520.build_test_suite_520("vga",  min_err_commits = 4, max_repo_files = 30)
-    tester.add_make_test("sim_mmcm")
+    tester.add_make_test("sim_vga")
     tester.add_make_test("sim_vga_top")
     tester.add_make_test("gen_bit")
     tester.add_filegen_test(["vga_top.bit",])
