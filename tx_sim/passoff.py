acee75fa308a9604c0f8af81edc77dba3d4e1625
Mike Wirthlin <wirthlin@byu.edu>
2025-08-27 14:38:12 -0600

    Added passoff

---
+++ tx_sim/passoff.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+# Manages file paths
+import pathlib
+import sys
+
+sys.dont_write_bytecode = True # Prevent the bytecodes for the resources directory from being cached
+# Add to the system path the "resources" directory relative to the script that was run
+resources_path = pathlib.Path(__file__).resolve().parent.parent  / 'resources'
+sys.path.append( str(resources_path) )
+
+import test_suite_520
+import repo_test
+
+def main():
+    tester = test_suite_520.build_test_suite_520("tx_sim", start_date="09/03/2025", max_repo_files = 20)
+    tester.add_required_tracked_files(["tx.sv","tx_sim.png", "tx_sim_long.png"])
+    tester.add_Makefile_rule("sim_tx", ["tx.sv","rx_model.sv","tx_tb.sv" ])
+    tester.add_Makefile_rule("sim_tx_115200_even", ["tx.sv","rx_model.sv","tx_tb.sv" ])
+    tester.run_tests()
+
+if __name__ == "__main__":
+    main()
+
