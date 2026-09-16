d68222b0e42c43e265efa9678706a83fc0a3f5a7
wirthlin <wirthlin@byu.edu>
2026-09-16 16:17:58 -0600

    Fixed a few download issues for rx_download

--- rx_download/passoff.py
+++ rx_download/passoff.py
@@ -19,9 +19,12 @@ def main():
     tester.add_Makefile_rule("synth_rxtx_top", ["rxtx_top.sv"], ["synth_rxtx_top.log", "rxtx_top_synth.dcp"])
     tester.add_Makefile_rule("synth_rxtx_top_115200_even", ["rxtx_top.sv"], ["synth_rxtx_top_115200_even.log",
                                                                             "rxtx_top_115200_even_synth.dcp"])
-    tester.add_Makefile_rule("implement_rxtx_top", ["rxtx_top_synth.dcp"], ["synth_rxtx_top.log", "rxtx_top.dcp", "rxtx_top.bit"])
+    tester.add_Makefile_rule("implement_rxtx_top", ["rxtx_top_synth.dcp"],
+                             ["implement_rxtx_top.log", "rxtx_top.dcp", "rxtx_top.bit",
+                              "utilization.rpt", "timing.rpt"])
     tester.add_Makefile_rule("implement_rxtx_top_115200_even", ["rxtx_top_115200_even_synth.dcp"], 
-                             ["synth_rxtx_top_115200_even.log", "rxtx_top_115200_even.dcp", "rxtx_top_115200_even.bit"])
+                             ["implement_rxtx_top_115200_even.log", "rxtx_top_115200_even.dcp", "rxtx_top_115200_even.bit",
+                              "utilization_115200_even.rpt", "timing_115200_even.rpt"])
     tester.run_tests()
 
 
